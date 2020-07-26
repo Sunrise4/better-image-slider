05e9c24c509d38ab85da2af11f13c761b5f3b350
Sunrise <sunrise4ws@gmail.com>
2020-07-25 21:10:17 -0500

    nav bar working

--- scripts/index.js
+++ scripts/index.js
@@ -25,14 +25,24 @@ function moveToSlide(track, currentSlide, targetSlide) {
   targetSlide.classList.add('carousel_slide--current-slide');
 }
 
+function updateDots(currentDot, targetDot) {
+  currentDot.classList.remove('carousel_indicator--current-slide');
+  targetDot.classList.add('carousel_indicator--current-slide');
+}
+
 //when i click left, move slides to the let
 prevButton.addEventListener('click', (e) => {
   const currentSlide = track.querySelector(
     '.carousel_slide--current-slide',
   );
   const prevSlide = currentSlide.previousElementSibling;
+  const currentDot = navBar.querySelector(
+    '.carousel_indicator--current-slide',
+  );
+  const prevDot = currentDot.previousElementSibling;
 
   moveToSlide(track, currentSlide, prevSlide);
+  updateDots(currentDot, prevDot);
 });
 //when i click right, move slides to the right
 nextButton.addEventListener('click', (e) => {
@@ -40,9 +50,32 @@ nextButton.addEventListener('click', (e) => {
     '.carousel_slide--current-slide',
   );
   const nextSlide = currentSlide.nextElementSibling;
+  const currentDot = navBar.querySelector(
+    '.carousel_indicator--current-slide',
+  );
+  const nextDot = currentDot.nextElementSibling;
 
   moveToSlide(track, currentSlide, nextSlide);
+  updateDots(currentDot, nextDot);
 });
 //when i click the nav indicators, move the the corresponding slide
 
+navBar.addEventListener('click', (e) => {
+  const targetDot = e.target.closest('button');
+  if (!targetDot) return;
+
+  const currentSlide = track.querySelector(
+    '.carousel_slide--current-slide',
+  );
+  const currentDot = navBar.querySelector(
+    '.carousel_indicator--current-slide',
+  );
+  const targetIndex = navDots.findIndex((dot) => dot === targetDot);
+  console.log(targetIndex);
+  const targetSlide = slides[targetIndex];
+
+  moveToSlide(track, currentSlide, targetSlide);
+
+  updateDots(currentDot, targetDot);
+});
 //36min on vid
